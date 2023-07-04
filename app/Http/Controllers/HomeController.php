f7db1c2dc57ca828dbad5b074346beaffa994bf3
Darshan H baraiya <damku999@users.noreply.github.com>
2023-07-05 01:10:43 +0530

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use App\Rules\MatchOldPassword;
+use App\Models\CustomerInsurance;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
@@ -28,7 +30,31 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $customers = Customer::all();
+        $total_customer = $customers->count();
+        $active_customer = $customers->where('status', 1)->count();
+        $inactive_customer =  $customers->where('status', 0)->count();
+
+        $customer_insurances = CustomerInsurance::all();
+        $total_customer_insurance = $customer_insurances->count();
+        $active_customer_insurance = $customer_insurances->where('status', 1)->count();
+        $inactive_customer_insurance =  $customer_insurances->where('status', 0)->count();
+        $expiring_customer_insurance =  $customer_insurances->where('status', 0)->filter(function ($user) {
+            $date = Carbon::parse($user->expired_date);
+            $date1 = Carbon::now();
+            return $date->month == $date1->month && $date->year == $date1->year;
+        })->count();
+
+        return view('home', [
+            'total_customer' => $total_customer,
+            'active_customer' => $active_customer,
+            'inactive_customer' => $inactive_customer,
+
+            'total_customer_insurance' => $total_customer_insurance,
+            'active_customer_insurance' => $active_customer_insurance,
+            'inactive_customer_insurance' => $inactive_customer_insurance,
+            'expiring_customer_insurance' => $expiring_customer_insurance,
+        ]);
     }
 
     /**
@@ -59,7 +85,7 @@ class HomeController extends Controller
 
         try {
             DB::beginTransaction();
-            
+
             #Update Profile Data
             User::whereId(auth()->user()->id)->update([
                 'first_name' => $request->first_name,
@@ -72,7 +98,6 @@ class HomeController extends Controller
 
             #Return To Profile page with success
             return back()->with('success', 'Profile Updated Successfully.');
-            
         } catch (\Throwable $th) {
             DB::rollBack();
             return back()->with('error', $th->getMessage());
@@ -97,14 +122,13 @@ class HomeController extends Controller
             DB::beginTransaction();
 
             #Update Password
-            User::find(auth()->user()->id)->update(['password'=> Hash::make($request->new_password)]);
-            
+            User::find(auth()->user()->id)->update(['password' => Hash::make($request->new_password)]);
+
             #Commit Transaction
             DB::commit();
 
             #Return To Profile page with success
             return back()->with('success', 'Password Changed Successfully.');
-            
         } catch (\Throwable $th) {
             DB::rollBack();
             return back()->with('error', $th->getMessage());

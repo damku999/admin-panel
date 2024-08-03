110426b36dbf5f6b23243fd602fe195c42172722
Darshan H baraiya <damku999@gmail.com>
2024-08-03 17:07:44 +0530

    fontawesome

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -15,8 +15,9 @@
     <link rel="shortcut icon" type="image/jpg" href="{{ asset('images/icon.png') }}" />
 
     <!-- Font Awesome UI KIT-->
-    <script src="https://kit.fontawesome.com/f75ab26951.js" crossorigin="anonymous"></script>
-
+    {{-- <script src="https://kit.fontawesome.com/f75ab26951.js" crossorigin="anonymous"></script> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/js/all.min.js" integrity="sha512-6sSYJqDreZRZGkJ3b+YfdhB3MzmuP9R7X1QZ6g5aIXhRvR1Y/N/P47jmnkENm7YL3oqsmI6AK+V6AD99uWDnIw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
